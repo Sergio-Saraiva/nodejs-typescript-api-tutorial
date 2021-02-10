d95f267ead232b382a33db74027c06cef73a55bb
Sérgio Saraiva <sergio.s.neto@outlook.com>
2021-02-10 14:29:11 -0300

    cap2/step2/handling errors

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,4 +1,6 @@
 import { AxiosStatic } from 'axios';
+import { stringify } from 'querystring';
+import { InternalError } from '../util/errors/internal-error';
 
 export interface StormGlassPointSource {
   [key: string]: number;
@@ -30,6 +32,22 @@ export interface ForecastPoint {
   windSpeed: number;
 }
 
+export class ClientRequestError extends InternalError{
+  constructor(message: string) {
+   const internalMessage = 'Unexpected error when trying to communicate to StormGlass';
+
+   super(`${internalMessage}: ${message}`)
+  }
+}
+
+export class StormGlassResponseError extends InternalError {
+  constructor(message: string) {
+    const internalMessage =
+      'Unexpected error returned by the StormGlass service';
+    super(`${internalMessage}: ${message}`);
+  }
+}
+
 export class StormGlass {
 
   readonly stormGlassAPIParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
@@ -37,7 +55,8 @@ export class StormGlass {
 
   constructor(protected request: AxiosStatic) {}
 
-  public async fecthPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+  public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+    try{
     const response = await this.request.get<StormGlassForecastResponse>(
       `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`,
       {
@@ -48,6 +67,13 @@ export class StormGlass {
     );
 
     return this.normalizeResponse(response.data);
+    }catch(err) {
+      if(err.response && err.response.status) {
+        throw new StormGlassResponseError(`Error: ${JSON.stringify(err.response.data)} Code: ${err.response.status}`)
+      }
+
+      throw new ClientRequestError(err.message);
+    }
   }
 
   private normalizeResponse(points: StormGlassForecastResponse): ForecastPoint[] {
